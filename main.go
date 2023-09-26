b56173fddcbb9692411a576a8a11d8adcfca94b9
Micah <micahshallom@gmail.com>
2023-09-26 12:50:36 +0100

    using context to spin up multiple servers on multiple ports

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	f "fmt"
 	"log"
 	"net/http"
+	"context"
+	"net"
 )
 
+const keyServerAddr = "ServerAddr"
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	f.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
 	if err := r.ParseForm(); err != nil {
 		f.Fprintf(w, "ParseForm() err: %v", err)
 		return
@@ -16,11 +22,15 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	f.Fprintf(w, "Post request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
-	f.Fprintf(w, "Name= %s\n", name)clearaddress
+	f.Fprintf(w, "Name= %s\n", name)
 	f.Fprintf(w, "Address= %s\n", address)
-}  
-
+	}  
+	
 func mainHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	f.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+
 	if r.URL.Path != "/main" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
@@ -35,20 +45,66 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./static"))
-	
+
 	mux.Handle("/", fileServer)
 	mux.HandleFunc("/form", formHandler)
 	mux.HandleFunc("/main", mainHandler)
 
-	f.Printf("Starting server on port 8000\n")
 	// if err := http.ListenAndServe(":8000", nil); err != nil {
+		// 	log.Fatal(err)
+		// }
+		
+		ctx, cancelCtx := context.WithCancel(context.Background())
+		
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+	
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+	
+	go func(){
+		f.Printf("Starting server on port 3333\n")
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed){
+			f.Printf("Server One has been shutdown")
+			}else if err != nil {
+			f.Printf("Error Starting Server One")
+			log.Fatal(err)
+		}
+		cancelCtx()
+		}()
+		
+		go func(){
+			f.Printf("Starting server on port 4444\n")
+			err := serverTwo.ListenAndServe()
+			if errors.Is(err, http.ErrServerClosed){
+				f.Printf("Server Two has been shutdown")
+				}else if err != nil {
+					f.Printf("Error Starting Server Two")
+			log.Fatal(err)
+		}
+		cancelCtx()
+	}()
+
+	<- ctx.Done()
+
+	// err := http.ListenAndServe(":8000", mux)
+	// if errors.Is(err, http.ErrServerClosed){ //checks if the server was shutdown
+	// 	f.Printf("Server has been shutdown")
+	// }else if err != nil {
+	// 	f.Printf("Error starting server")
 	// 	log.Fatal(err)
 	// }
-	err := http.ListenAndServe(":8000", mux)
-	if errors.Is(err, http.ErrServerClosed){ //checks if the server was shutdown
-		fmt.Printf("Server has been shutdown")
-	}else if err != nil {
-		fmt.Printf("Error starting server")
-		log.Fatal(err)
-	}
 }
